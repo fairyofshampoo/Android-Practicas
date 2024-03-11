6630a727b58804ad07b6c5d77710ed78852954e8
ᓚᘏᗢ michelle ! <96703919+fairyofshampoo@users.noreply.github.com>
2024-03-10 21:13:51 -0600

    agregando el de earthquakes

---
+++ Earthquakes/app/src/main/java/com/ikariscraft/earthquakes/ApiClient.java
@@ -0,0 +1,37 @@
+package com.ikariscraft.earthquakes;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+import retrofit2.http.GET;
+
+public class ApiClient {
+
+    public interface Service{
+        @GET("all_hour.geojson")
+        Call<String> getEarthquakes();
+    }
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build();
+
+    Service service;
+    private static final ApiClient apiClient = new ApiClient();
+
+    public static ApiClient getInstance(){
+        return apiClient;
+    }
+
+    private ApiClient(){
+
+    }
+
+    public Service getService(){
+        if(service == null){
+            service = retrofit.create(Service.class);
+        }
+        return service;
+    }
+}
